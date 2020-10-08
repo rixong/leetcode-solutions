e4cb2bb922894cd63276270bcea073d922b9de58
rixong <rixong@gmail.com>
2020-10-08 15:01:07 -0500

    rotate right success

--- index.js
+++ index.js
@@ -31,76 +31,46 @@ Calculate new last node by subtracting n from length.
 
 */
 
-// class Node {
-//   constructor(data, next = null) {
-//     this.data = data;
-//     this.next = next;
-//   }
-// }
-
-// class LinkedList {
-//   constructor() {
-//       this.head = null;
-//   }
-//   insertFirst(data) {
-//       this.head = new Node(data, this.head);
-//   }
-
-//   insertLast(data) {
-//     const node = new Node(data);
-//     const last = this.getLast();
-//     last ? last.next = node : this.head = node
-//   }
-
-//   getLast() {
-//     if (!this.head) {
-//       return null
-//     }
-//     let node = this.head;
-//     while (node) {
-//       if (!node.next) {
-//         return node
-//       }
-//       node = node.next;
-//     }
-//   }
-
-//   size() {
-//     let node = this.head;
-//     let counter = 0;
-//     while (node) {
-//       counter++;
-//       node = node.next;
-//     }
-//     return counter;
-//   }
-
-// }
-
 var rotateRight = function (head, k) {
-  //Find size:
-  let size = 0;
-  let node = head;
-  while (node) {
-    size++
-    node = node.next
-  }
-  const endNode = k % size;
+  if (!head || !k){
+    return head;
+}
 
-  // Break and connect end to head
-  node = head;   //i = 0    endNode = 2
-  for (let i = 0; i < size; i++) {
-    if (i = endNode) {
-      tail = node
-      newHead = node.next
-    }
-    if (!node.next) {
-      node.next = head;
-      tail.next = null
-    }
-    node = node.next
-  }
-  return newhead
+//Find size:
+let size = 0;
+let node = head;
+
+while (node) {
+size++
+node = node.next
+}
+
+const offset = k % size;  //1
+
+if (offset === 0){
+    return head
+}
+
+// Break and connect end to head
+
+let newHead;
+let tail;
+let tailIndex = size - offset - 1;
+
+node = head;
+
+for (let i = 0; i < size; i++) {
+if (i === tailIndex) {
+  tail = node
+  newHead = node.next
+}
+if (!node.next) {
+  node.next = head;
+  tail.next = null
+}
+node = node.next
+}
+return newHead
 
 }
 
